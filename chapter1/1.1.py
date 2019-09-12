afa35f5e126482c3479c924467cb6249efed56c0
Whiteyingshan <a1746345173@163.com>
2019-09-12 16:15:57 +0800

    Class Over

--- chapter1/1.1.py
+++
@@ -1,4 +0,0 @@
-radius=25
-area = 3.1415*radius*radius
-print(area)
-print("{:.2f}".format(area))
---
+++ chapter1/微实例1.1.py
@@ -0,0 +1,4 @@
+radius = 25
+area = 3.1415 * radius * radius
+print(area)
+print("{:.2f}".format(area))
